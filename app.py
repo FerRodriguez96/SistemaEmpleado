6e021d828e28eaa5f56962fce0797ef074d8b08a
Fernando <feer.rodriguez96@gmail.com>
2021-09-18 19:32:29 -0300

    #2 dev - funcionalidad agregar
    Se agrego la funcion de almacenamiento para guardar datos de form en la base de datos

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from logging import debug
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 
@@ -17,17 +17,29 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql ="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'fernando', 'fernando@fernando', 'foto.jpg');"
-    conn = mysql.connect()
-    cursor= conn.cursor()
-    cursor.execute(sql)
-    conn.commit()
+   
 
     return render_template('empleados/index.html')
 
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+
+    _nombre = request.form["txtNombre"]
+    _correo = request.form["txtCorreo"]
+    _foto = request.files["txtFoto"]
+
+    datos =(_nombre,_correo,_foto.filename)
+    sql ="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    conn = mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
