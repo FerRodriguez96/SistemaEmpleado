a4f732c56a1fa91062da372226aad441739e1ae3
Fernando <feer.rodriguez96@gmail.com>
2021-09-18 01:54:07 -0300

    #1 dev - Formulario create
    Se agrego el formulario basico para ingresar un nuevo empleado

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from logging import debug
 from flask import Flask
 from flask import render_template
+from flaskext.mysql import MySQL
 
 
 app= Flask(__name__)
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='sistema'
+mysql.init_app(app)
+
+
 @app.route('/')
 def index():
+
+    sql ="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'fernando', 'fernando@fernando', 'foto.jpg');"
+    conn = mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
     return render_template('empleados/index.html')
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
