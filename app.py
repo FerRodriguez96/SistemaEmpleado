b96f3e0790c3d49105d483ef5247d0a2d4089c2f
Fernando <feer.rodriguez96@gmail.com>
2021-09-20 23:42:14 -0300

    #6 dev - Funcion modificar
    Se ha añadido la funcion modificar y la posibilidad de modificar la foto asociada con el campo.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
-
+import os
 
 
 app= Flask(__name__)
@@ -15,6 +15,8 @@ app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema'
 mysql.init_app(app)
 
+Carpeta =os.path.join('uploads')
+app.config['Carpeta']=Carpeta
 
 @app.route('/')
 def index():
@@ -33,14 +35,65 @@ def destroy(id):
     conn=mysql.connect()
     cursor=conn.cursor()
 
+    cursor.execute("SELECT foto FROM empleados WHERE id=%s",id)
+    fila=cursor.fetchall()
+
+    os.remove(os.path.join(app.config['Carpeta'],fila[0][0]))
     cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
+    
     conn.commit()
 
     return redirect('/')
 
+@app.route('/edit/<int:id>')
+def edit(id):
+    conn = mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id=%s", (id))
+    empleados=cursor.fetchall()
+    conn.commit()
+
+    return render_template('empleados/edit.html',empleados=empleados)
+
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form["txtNombre"]
+    _correo = request.form["txtCorreo"]
+    _foto = request.files["txtFoto"]
+
+    id=request.form['txtID']
+
+    sql ="UPDATE empleados SET nombre=%s, correo=%s WHERE id=%s;"
+
+    datos =(_nombre,_correo,id)
+
+    conn = mysql.connect()
+    cursor= conn.cursor()
+
+    now = datetime.now()
+    tiempo= now.strftime("%Y%H%M$S")
+
+    if _foto.filename!="":
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM empleados WHERE id=%s",id)
+        fila=cursor.fetchall()
+
+        os.remove(os.path.join(app.config['Carpeta'],fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s",(nuevoNombreFoto,id))
+        conn.commit()
+
+
+    cursor.execute(sql,datos)
+
+    conn.commit()
+
+    return redirect('/')
 
 @app.route('/store', methods=['POST'])
 def storage():
